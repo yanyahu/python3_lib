8cae8a62467ca4747136af74512f6de8eec3cf56
yanyahu <yahuyan@163.com>
2021-10-05 14:08:46 +0800

    linecache

--- linecache_mod/linecache_getline.py
+++ linecache_mod/linecache_getline.py
@@ -1,2 +1,14 @@
-# TODO
-# https://pymotw.com/3/linecache/index.html
+import linecache
+from linecache_data import *
+
+filename = make_tempfile()
+
+# Pick out the same line from source and cache.
+# (Notice that linecache counts from 1)
+print('SOURCE:')
+print('{!r}'.format(lorem.split('\n')[4]))
+print()
+print('CACHE:')
+print('{!r}'.format(linecache.getline(filename, 5)))
+
+cleanup(filename)
