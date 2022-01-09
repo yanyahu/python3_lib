221e7c2292b2ae451491ef03337b389789a812c5
yanyahu <yahuyan@163.com>
2022-01-10 00:10:53 +0800

    xml

--- xml/ElementTree_find_feeds_by_tag.py
+++
@@ -1,9 +0,0 @@
-from xml.etree import ElementTree
-
-with open('podcasts.opml', 'rt') as f:
-    tree = ElementTree.parse(f)
-
-for node in tree.findall('.//outline'):
-    url = node.attrib.get('xmlUrl')
-    if url:
-        print(url)
--- xml/ElementTree_parse_opml.py
+++
@@ -1,6 +0,0 @@
-from xml.etree import ElementTree
-
-with open('podcasts.opml', 'rt') as f:
-    tree = ElementTree.parse(f)
-
-print(tree)
