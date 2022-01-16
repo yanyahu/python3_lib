2c60028a53b18b3f91dc8699d6ae9287765a5542
yanyahu <yahuyan@163.com>
2022-01-17 00:07:30 +0800

    asyncio

--- asyncio_mod/asyncio_echo_server_protocol.py
+++ asyncio_mod/asyncio_echo_server_protocol.py
@@ -1,4 +1,15 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import asyncio
+import logging
+import sys
 
+SERVER_ADDRESS = ('localhost', 10000)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(name)s: %(message)s',
+    stream=sys.stderr,
+)
+log = logging.getLogger('main')
+
+event_loop = asyncio.get_event_loop()
 # TODO https://pymotw.com/3/asyncio/io_protocol.html
