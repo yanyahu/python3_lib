0679d91c5a7ec4dd0093c968d11c2333f081fe00
yanyahu <yahuyan@163.com>
2022-01-18 23:16:02 +0800

    argparse

---
+++ argparse_mod/argparse_short.py
@@ -0,0 +1,12 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Short sample app')
+
+parser.add_argument('-a', action="store_true", default=False)
+parser.add_argument('-b', action="store", dest="b")
+parser.add_argument('-c', action="store", dest="c", type=int)
+
+print(parser.parse_args(['-a', '-bval', '-c', '3']))
+
+
+# TODO https://pymotw.com/3/argparse/index.html
