1f5ccddeee14cc54148dbbe34d619411b0727615
yanyahu <yahuyan@163.com>
2022-01-20 19:21:37 +0800

    traceback

---
+++ traceback_mod/traceback_print_stack.py
@@ -0,0 +1,16 @@
+import traceback
+import sys
+
+from traceback_example import call_function
+
+
+def f():
+    traceback.print_stack(file=sys.stdout)
+
+
+print('Calling f() directly:')
+f()
+
+print()
+print('Calling f() from 3 levels deep:')
+call_function(f)
