9a63be62f3a57335e093a196772380cde6700ef6
yanyahu <yahuyan@163.com>
2022-01-20 13:57:03 +0800

    unittest

---
+++ unittest_mod/unittest_simple.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class SimplisticTest(unittest.TestCase):
+
+    def test(self):
+        a = 'a'
+        b = 'a'
+        self.assertEqual(a, b)
+
+"""
+$ python3 -m unittest unittest_simple.py
+.
+----------------------------------------------------------------
+Ran 1 test in 0.000s
+
+OK
+"""
+
+# TODO https://pymotw.com/3/unittest/index.html
