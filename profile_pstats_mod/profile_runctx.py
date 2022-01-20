6d24f73550887880108b3d88b6f9a1b85d9b9134
yanyahu <yahuyan@163.com>
2022-01-20 22:31:33 +0800

    profile_pstats

---
+++ profile_pstats_mod/profile_runctx.py
@@ -0,0 +1,9 @@
+import profile
+from profile_fibonacci_memoized import fib, fib_seq
+
+if __name__ == '__main__':
+    profile.runctx(
+        'print(fib_seq(n)); print()',
+        globals(),
+        {'n': 20},
+    )
