8929bd1259a9e3ce66032cf970fb8f7ed912e15e
yanyahu <yahuyan@163.com>
2022-01-20 11:17:27 +0800

    configparser

---
+++ configparser_mod/configparser_escape.py
@@ -0,0 +1,10 @@
+from configparser import ConfigParser
+import os
+
+filename = 'escape.ini'
+config = ConfigParser()
+config.read([filename])
+
+value = config.get('escape', 'value')
+
+print(value)
