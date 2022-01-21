d6f7f23822703450afa3c59851893c26e783c0c9
yanyahu <yahuyan@163.com>
2022-01-21 11:49:14 +0800

    os

---
+++ os_mod/os_scandir.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+for entry in os.scandir(sys.argv[1]):
+    if entry.is_dir():
+        typ = 'dir'
+    elif entry.is_file():
+        typ = 'file'
+    elif entry.is_symlink():
+        typ = 'link'
+    else:
+        typ = 'unknown'
+    print('{name} {typ}'.format(
+        name=entry.name,
+        typ=typ,
+    ))
---
+++ os_mod/os_system_shell.py
@@ -0,0 +1,4 @@
+import os
+
+# Command with shell expansion
+os.system('echo $TMPDIR')
