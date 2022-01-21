a9094876965dc786ea2f9865d9a67d613a32c87c
yanyahu <yahuyan@163.com>
2022-01-21 15:50:09 +0800

    warnings

---
+++ warnings_mod/warnings_filter.py
@@ -0,0 +1,4 @@
+import warnings
+
+warnings.warn('Show this message')
+warnings.warn('Do not show this message')
---
+++ warnings_mod/warnings_warn.py
@@ -0,0 +1,5 @@
+import warnings
+
+print('Before the warning')
+warnings.warn('This is a warning message')
+print('After the warning')
